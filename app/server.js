a1cf0068a2c21dcf194f5ad662859f31c0e3bb13
jonathanduvan <jonathan.d.gonzalez.18@dartmouth.edu>
2016-08-14 22:51:42 -0400

    ugh port stuff

--- app/server.js
+++ app/server.js
@@ -34,7 +34,9 @@ app.get('/', (req, res) => {
 
 // START THE SERVER
 // =============================================================================
-const port = process.env.PORT || 9090;
-app.listen(port);
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
+});
 
 console.log(`listening on: ${port}`);
